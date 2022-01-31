d681443f40aaa9c2e344f0de93caf28648e12b82
idevnerds-Arslan <arslan@idevnerds.com>
2022-01-31 19:02:35 +0500

    update cors port

--- app.mjs
+++ app.mjs
@@ -21,7 +21,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
 const corsOptions = {
   credentials: true,
-  origin: ['http://localhost:4001']
+  origin: ['http://localhost:3000']
 }
 app.use(logger('dev'))
 app.use(express.json())
